5c29a512633ce724d808e42266872bfdba414577
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-02-28 10:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+let strongTexts = document.querySelectorAll("strong")
+
+
 function highlight() {
     //Write your code here
-
+for(let s of strongTexts){
+	s.style.color = "rgb(0, 128, 0)"
+}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+for(let s of strongTexts){
+	s.style.color = "rgb(0, 0, 0)"
+}
     
 }
